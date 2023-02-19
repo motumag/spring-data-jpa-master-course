c34dc6b5d38c6fe7b1150159e284f5cb6dd6d6ba
motumag <motumagishu27@gmail.com>
2023-02-19 12:19:09 +0300

    jpql query added

--- src/main/java/com/motuma/jpamastercourse/StudentRepository.java
+++ src/main/java/com/motuma/jpamastercourse/StudentRepository.java
@@ -1,13 +1,16 @@
 package com.motuma.jpamastercourse;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface StudentRepository extends JpaRepository<Student, Long> {
     //How to use custom query
+    @Query("SELECT s FROM Student s where s.email=?1 ") //Jpql query
     Optional<Student> findStudentByEmail(String email);
+    @Query("SELECT s FROM Student s WHERE s.firstName=?1 AND s.age=?2")
     List<Student > findStudentByFirstNameEqualsAndAgeEquals(String firstName,Integer age);
 
 }
