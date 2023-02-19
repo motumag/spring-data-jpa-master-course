1ef1cf634c106b1a28456538440f7ed1e74c1e11
motumag <motumagishu27@gmail.com>
2023-02-19 03:26:08 +0300

    spring data jpa master course

---
+++ src/main/java/com/motuma/jpamastercourse/JpaMasterCourseApplication.java
@@ -0,0 +1,13 @@
+package com.motuma.jpamastercourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaMasterCourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaMasterCourseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/motuma/jpamastercourse/JpaMasterCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.motuma.jpamastercourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaMasterCourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
